7845642e7b4816ad65ea48c32d500c5bcfed5f1b
Andy Gainey <7810015+againey@users.noreply.github.com>
2015-11-23 12:16:07 -0600

    Attempt to redesign the editor interface architecture.  It's not really working.  Unity, ironically enough, doesn't really appreciate hierarchical modularity.  Update order and serialization are a complete mess.  Everything I try to do to make it work with Unity is really just a nasty hack to work around Unity. Also includes the work on adding regions and topology visitation algorithms.  But it's all mixed in together in a crazy way.  Needs untangling.

---
+++ Assets/Scripts/Experilous/IRefreshable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Experilous
+{
+	public delegate void AwaitingRefreshEventHandler(IRefreshable refreshable);
+	public delegate void RefreshedEventHandler(IRefreshable refreshable, bool wasAwaitingRefresh);
+
+	public interface IRefreshable
+	{
+		event AwaitingRefreshEventHandler AwaitingRefresh;
+		event RefreshedEventHandler Refreshed;
+
+		bool Refresh();
+		bool RefreshImmediately();
+		bool RefreshImmediatelyIfAwaiting();
+		bool isAwaitingRefresh { get; }
+
+		void PropagateRefresh(IRefreshable source, bool wasAwaitingRefresh);
+	}
+}
---
+++ Assets/Scripts/Experilous/PropertyChangedNotifier.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.ComponentModel;
+
+namespace Experilous
+{
+	public class PropertyChangedNotifier : MonoBehaviour, INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void NotifyPropertyChanged(string propertyName)
+		{
+			var handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
