9614eac1f6c2f89c970aeb95060ddaac66fe1310
Andy Gainey <7810015+againey@users.noreply.github.com>
2015-11-25 14:51:20 -0600

    Replaced the multi-component modular design with a monolithic single component design, where the TopologyGenerator alone can be attached to a game object and handle all the multifaceted details of generating a topology and related data and objects. Added an IRandomEngine interface, an abstract buffered helper that can receive multiple bits of randomness and dole them out in different increments, a full implementation based on System.Random, a MonoBehaviour engine factory, and a helper class with all the fancier random functions built on top of an engine. Made a variety of topology objects serializable so that they don't have to be regenerated from scratch when loading a scene. Topology attribute fields will now return 0 instead of throw an exception if they don't have any values. Topology attribute fields all support being reset, offer an isEmpty property, and expose their underlying array as a get-only property. Various classes that had been previously creating an internal System.Random instance now take an IRandomEngine as a parameter. Hexahedrons partially renamed back to cubes; no need to be cute. Radically reworked the edge pivoting and spinning to differentiate between vertex and face edges.  Doing operations on vertex edges focuses on shifting the neighbors of vertices; on face edges, the focus on on shifting the neighbors of faces. Relaxed topology constraints when pivoting or spinning edges.  Vertices and faces only need at least 2 neighbors, not 3, and boundary edges are irrelevant. Work on face/path mesh generators and mouse/ray pickers is still outstanding.

---
+++ Assets/Scripts/Experilous/BufferedRandomEngine.cs
@@ -0,0 +1,127 @@
+﻿namespace Experilous
+{
+	public abstract class BufferedRandomEngine : IRandomEngine
+	{
+		private uint _buffer = 0;
+		private int _bufferBitCount = 0;
+
+		private static sbyte[] _log2CeilLookupTable = // Table[i] = Ceil(Log2(i))
+		{
+			0, 0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4,
+			4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+			5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+			6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+			6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+			7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+		};
+
+		private static sbyte[] _plus1Log2CeilLookupTable = // Table[i] = Ceil(Log2(i+1))
+		{
+			0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4,
+			5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+			6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+			6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+		};
+
+		public abstract uint Next();
+
+		public uint NextBits(int bitCount)
+		{
+			if (_bufferBitCount < bitCount)
+			{
+				int existingBitCount = bitCount - _bufferBitCount;
+				int remainingBitCount = 32 - existingBitCount;
+				uint next = Next();
+				uint random = (_buffer << existingBitCount) | (next >> remainingBitCount);
+				_buffer = next & ((1U << remainingBitCount) - 1U);
+				_bufferBitCount = remainingBitCount;
+				return random;
+			}
+			else
+			{
+				int remainingBitCount = _bufferBitCount - bitCount;
+				uint random = _buffer >> remainingBitCount;
+				_buffer = _buffer & ((1U << remainingBitCount) - 1U);
+				_bufferBitCount = remainingBitCount;
+				return random;
+			}
+		}
+
+		private static int Log2Ceil(uint n)
+		{
+			var high16 = n >> 16;
+			if (high16 != 0)
+			{
+				var high8 = high16 >> 8;
+				return (high8 != 0) ? 24 + _log2CeilLookupTable[high8] : 16 + _log2CeilLookupTable[high16];
+			}
+			else
+			{
+				var high8 = n >> 8;
+				return (high8 != 0) ? 8 + _log2CeilLookupTable[high8] : _log2CeilLookupTable[n];
+			}
+		}
+
+		private static int Plus1Log2Ceil(uint n)
+		{
+			var high16 = n >> 16;
+			if (high16 != 0)
+			{
+				var high8 = high16 >> 8;
+				return (high8 != 0) ? 24 + _plus1Log2CeilLookupTable[high8] : 16 + _plus1Log2CeilLookupTable[high16];
+			}
+			else
+			{
+				var high8 = n >> 8;
+				return (high8 != 0) ? 8 + _plus1Log2CeilLookupTable[high8] : _plus1Log2CeilLookupTable[n];
+			}
+		}
+
+		public uint NextLessThan(uint upperBound)
+		{
+			if (upperBound == 0) throw new System.ArgumentOutOfRangeException("upperBound");
+			var bitsNeeded = Log2Ceil(upperBound);
+			uint random;
+			do
+			{
+				random = NextBits(bitsNeeded);
+			}
+			while (random >= upperBound);
+			return random;
+		}
+
+		public uint NextLessThanOrEqual(uint upperBound)
+		{
+			var bitsNeeded = Plus1Log2Ceil(upperBound);
+			uint random;
+			do
+			{
+				random = NextBits(bitsNeeded);
+			}
+			while (random > upperBound);
+			return random;
+		}
+	}
+}
