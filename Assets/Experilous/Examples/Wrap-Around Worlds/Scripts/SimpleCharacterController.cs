66510e97ce05253d68b59ad1f34f6f3d0df5303b
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-02-23 23:30:36 -0600

    Added a copyright and license notice header to the top of every source code file. Added a license agreement text file describing an evaluational license. Removed a few more unnecessary using statements.

--- Assets/Experilous/Examples/Wrap-Around Worlds/Scripts/SimpleCharacterController.cs
+++ Assets/Experilous/Examples/Wrap-Around Worlds/Scripts/SimpleCharacterController.cs
@@ -1,5 +1,12 @@
-﻿using UnityEngine;
-using Experilous.WrapAround;
+﻿/******************************************************************************\
+ *  Copyright (C) 2016 Experilous <againey@experilous.com>
+ *  
+ *  This file is subject to the terms and conditions defined in the file
+ *  'Assets/Plugins/Experilous/License.txt', which is a part of this package.
+ *
+\******************************************************************************/
+
+using UnityEngine;
 
 namespace Experilous.Examples.WrapAround
 {
--- Assets/Plugins/Experilous/WrapAround/LightElementGhost.cs
+++ Assets/Plugins/Experilous/WrapAround/LightElementGhost.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿/******************************************************************************\
+ *  Copyright (C) 2016 Experilous <againey@experilous.com>
+ *  
+ *  This file is subject to the terms and conditions defined in the file
+ *  'Assets/Plugins/Experilous/License.txt', which is a part of this package.
+ *
+\******************************************************************************/
 
 namespace Experilous.WrapAround
 {
